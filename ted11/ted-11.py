969b140e48969909ddf21e0ac03e38b2bd275140
Nayara Oliveira <103392198+OliveiraNayara@users.noreply.github.com>
2022-05-04 18:09:57 -0300

    Add files via upload

---
+++ ted11/ted-11.py
@@ -0,0 +1,44 @@
+
+import random
+
+matriz_10x10a = []
+
+for i in range(10):
+    
+    matriz_aux = []
+    
+    for j in range(10):    
+        matriz_aux.append(random.randint(0, 100))
+    
+    matriz_10x10a.append(matriz_aux)        
+
+
+for linha in matriz_10x10a:
+    print(linha)
+    
+print('*' * 100)
+    
+resultado_soma = 0
+
+for l in matriz_10x10a:
+    
+    for c in l:
+        
+        resultado_soma += c
+
+print(f'A soma de todos os números da matriz A é = {resultado_soma}')
+
+matriz_10x10b = []
+
+for l in range(0, len(matriz_10x10a)):
+    
+    lista_auxiliar = []
+    
+    for c in range(0, len(matriz_10x10a[l])):
+        
+        resultado_multiplicacao = matriz_10x10a[l][c] * 3
+        lista_auxiliar.append(resultado_multiplicacao)
+        
+    matriz_10x10b.append(lista_auxiliar)
+        
+print(matriz_10x10b)
